fbda4965965cd9bfeef354799c607446fbb65bf6
ghyathCode <gheath.farkh@gmail.com>
2020-10-25 15:40:57 +0300

    Add city getter inside storeAdapter

--- storeAdapter.py
+++ storeAdapter.py
@@ -1,3 +1,4 @@
+from cityAdapter import CityAdapter
 from filterable import Filterable
 from reportable import Reportable
 
@@ -11,3 +12,12 @@ class StoreAdapter(Reportable, Filterable):
     def filter(self, ids):
         self.df = self.df[self.df['id'].isin(ids)]
         return self
+
+    def get_cities(self):
+        quantities = self.__get_city_quantities()
+
+        return CityAdapter(quantities).df
+
+    def __get_city_quantities(self):
+        ids = self.df[["city", "quantity"]].groupby(by="city").sum()
+        return ids.reset_index(level=0)
