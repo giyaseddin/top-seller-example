fd888f5187f82c4184556b27d73a424acd9027d9
ghyathCode <gheath.farkh@gmail.com>
2020-10-25 09:39:06 +0300

    Add test case to the top seller case given in the example

---
+++ solution.py
@@ -0,0 +1,10 @@
+def get_top_sellers_report(min_date="2020-01-01", max_date="2020-06-30", top=3):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- solutionTest.py
+++ solutionTest.py
@@ -1,9 +1,30 @@
 import unittest
+from solution import *
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class TopSellerTest(unittest.TestCase):
+    def test_solution_with_the_given_case(self):
+        result = get_top_sellers_report(min_date="2020-02-01", max_date="2020-06-30", top=2)
+        self.assertEqual(result,
+"-- top seller product --\
+  name  quantity\
+ p-103        33\
+ p-102        24\
+ p-110        24\
+-- top seller store --\
+name  quantity\
+ s-3        42\
+ s-2        36\
+ s-7        36\
+-- top seller brand --\
+    brand  quantity\
+ yoyodyne       100\
+     acme        65\
+-- top seller city --\
+      city  quantity\
+    gotham       108\
+ coruscant        78")
+
 
 
 if __name__ == '__main__':
