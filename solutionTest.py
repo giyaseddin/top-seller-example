1241346fac9373cd0f5312411884b3691d0b9751
ghyathCode <gheath.farkh@gmail.com>
2020-10-25 17:27:14 +0300

    Fix output in test

--- solutionTest.py
+++ solutionTest.py
@@ -1,29 +1,13 @@
 import unittest
 from solution import *
 
+with open('expected_output.txt', 'r') as file:
+    expected = file.read()
 
 class TopSellerTest(unittest.TestCase):
     def test_solution_with_the_given_case(self):
         result = generate_top_sellers_report(min_date="2020-02-01", max_date="2020-06-30", top=2)
-        self.assertEqual(result,
-"-- top seller product --\
-  name  quantity\
- p-103        33\
- p-102        24\
- p-110        24\
--- top seller store --\
-name  quantity\
- s-3        42\
- s-2        36\
- s-7        36\
--- top seller brand --\
-    brand  quantity\
- yoyodyne       100\
-     acme        65\
--- top seller city --\
-      city  quantity\
-    gotham       108\
- coruscant        78")
+        self.assertEquals(result, expected)
 
 
 
