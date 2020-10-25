2b4068e05ddba7b3d45cf350fa26a047c3f3b513
ghyathCode <gheath.farkh@gmail.com>
2020-10-25 15:37:02 +0300

    Add brand getter inside productAdapter

--- productAdapter.py
+++ productAdapter.py
@@ -1,13 +1,23 @@
+from brandAdapter import BrandAdapter
 from filterable import Filterable
 from reportable import Reportable
 
 
 class ProductAdapter(Reportable, Filterable):
-    file_name = 'product.csv'
+    file_name = "product.csv"
 
     def get_top_seller_report(self, top):
         return self.df.head(top)
 
     def filter(self, ids):
-        self.df = self.df[self.df['id'].isin(ids)]
+        self.df = self.df[self.df["id"].isin(ids)]
         return self
+
+    def get_brands(self):
+        quantities = self.__get_brand_quantities()
+
+        return BrandAdapter(quantities).df
+
+    def __get_brand_quantities(self):
+        ids = self.df[["brand", "quantity"]].groupby(by="brand").sum()
+        return ids.reset_index(level=0)
--- salesAdapter.py
+++ salesAdapter.py
@@ -5,21 +5,19 @@ from storeAdapter import StoreAdapter
 
 
 class SalesAdapter(Reportable):
-    file_name = 'sales.csv'
+    file_name = "sales.csv"
 
     def get_top_seller_report(self, top):
         return self.df.head(top)
 
     def between(self, min_date, max_date):
-        self.df = self.df[(self.df['date'] >= min_date) & (self.df['date'] <= max_date)]
+        self.df = self.df[(self.df["date"] >= min_date) & (self.df["date"] <= max_date)]
 
         return self
 
     def get_products(self):
-        ids = self.__get_product_ids()
-        quantities = self.__get_product_quantities(ids)
-
-        return ProductAdapter().filter(ids.index).set_quantities(quantities).df
+        quantities = self.__get_product_quantities()
+        return ProductAdapter().filter(quantities["id"]).set_quantities(quantities).df
 
     def get_stores(self):
         ids = self.__get_store_ids()
@@ -28,12 +26,13 @@ class SalesAdapter(Reportable):
         return StoreAdapter().filter(ids.index).set_quantities(quantities).df
 
     def __get_product_ids(self):
-        return self.df[["product", "quantity"]].groupby(by="product").sum()
+        return
 
     def __get_store_ids(self):
         return self.df[["store", "quantity"]].groupby(by="store").sum()
 
-    def __get_product_quantities(self, ids):
+    def __get_product_quantities(self):
+        ids = self.df[["product", "quantity"]].groupby(by="product").sum()
         return self.__get_related_quantities("product", ids)
 
     def __get_store_quantities(self, ids):
@@ -41,4 +40,4 @@ class SalesAdapter(Reportable):
 
     def __get_related_quantities(self, related, ids):
         quantities = ids.reset_index(level=0)
-        return quantities.rename(columns={related: 'id'})
+        return quantities.rename(columns={related: "id"})
