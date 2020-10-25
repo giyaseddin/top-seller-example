bc9efa263f24a98628da153e0b92a2d363b0b1da
ghyathCode <gheath.farkh@gmail.com>
2020-10-25 12:44:37 +0300

    Fix abstract reportable class constructor

--- reportable.py
+++ reportable.py
@@ -9,6 +9,9 @@ class Reportable:
     @abstractmethod
     def get_top_seller_report(self, top): raise NotImplementedError
 
-    def __init__(self):
-        self.file_path = 'datasets/' + self.file_name
-        self.df = ps.read_csv(self.file_path)
+    def __init__(self, df=None):
+        if df is None:
+            self.file_path = 'datasets/' + self.file_name
+            self.df = ps.read_csv(self.file_path)
+        else:
+            self.df = df
